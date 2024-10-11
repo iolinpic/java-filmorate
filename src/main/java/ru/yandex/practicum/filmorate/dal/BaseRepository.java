e0251f6cfac1d4f598b62bd81daf8e091f6d625c
iolinpic <iolinpic@gmail.com>
2024-10-11 22:50:53 +0300

    Обновление базовых значений

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/BaseRepository.java
@@ -0,0 +1,60 @@
+package ru.yandex.practicum.filmorate.dal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class BaseRepository<T> {
+    protected final JdbcTemplate jdbc;
+    protected final RowMapper<T> mapper;
+
+    protected T findOne(String query, Object... params) {
+        try {
+            return jdbc.queryForObject(query, mapper, params);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    protected List<T> findMany(String query, Object... params) {
+        return jdbc.query(query, mapper, params);
+    }
+
+    protected boolean delete(String query, long id) {
+        int rowsDeleted = jdbc.update(query, id);
+        return rowsDeleted > 0;
+    }
+
+    protected long insert(String query, Object... params) {
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            for (int idx = 0; idx < params.length; idx++) {
+                ps.setObject(idx + 1, params[idx]);
+            }
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKeyAs(Long.class);
+
+        if (id != null) {
+            return id;
+        } else {
+            throw new RuntimeException("Не удалось сохранить данные");
+        }
+    }
+
+    protected void update(String query, Object... params) {
+        int rowsUpdated = jdbc.update(query, params);
+        if (rowsUpdated == 0) {
+            throw new RuntimeException("Не удалось обновить данные");
+        }
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,52 +1,52 @@
-CREATE TABLE IF NOT EXISTS "users"
+CREATE TABLE IF NOT EXISTS users
 (
-    "id"       bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "email"    varchar,
-    "login"    varchar,
-    "name"     varchar,
-    "birthday" timestamp
+    id       bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email    varchar,
+    login    varchar,
+    name     varchar,
+    birthday timestamp
 );
 
-CREATE TABLE IF NOT EXISTS "user_friend"
+CREATE TABLE IF NOT EXISTS user_friend
 (
-    "user_id"   bigint,
-    "friend_id" bigint,
-    "status"    tinyint DEFAULT 0,
-    PRIMARY KEY ("user_id", "friend_id")
+    user_id   bigint,
+    friend_id bigint,
+    status    tinyint DEFAULT 0,
+    PRIMARY KEY (user_id, friend_id)
 );
 
-CREATE TABLE IF NOT EXISTS "films"
+CREATE TABLE IF NOT EXISTS films
 (
-    "id"          bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name"        varchar,
-    "description" text,
-    "releaseDate" timestamp,
-    "duration"    int,
-    "rating_id"   bigint
+    id          bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name        varchar,
+    description text,
+    releaseDate timestamp,
+    duration    int,
+    rating_id   bigint
 );
 
-CREATE TABLE IF NOT EXISTS "film_like"
+CREATE TABLE IF NOT EXISTS film_like
 (
-    "user_id" bigint,
-    "film_id" bigint,
-    PRIMARY KEY ("user_id", "film_id")
+    user_id bigint,
+    film_id bigint,
+    PRIMARY KEY (user_id, film_id)
 );
 
-CREATE TABLE IF NOT EXISTS "genre"
+CREATE TABLE IF NOT EXISTS genre
 (
-    "id"  bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" varchar
+    id  bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar
 );
 
-CREATE TABLE IF NOT EXISTS "rating"
+CREATE TABLE IF NOT EXISTS mpa
 (
-    "id"   bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" varchar
+    id   bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar
 );
 
-CREATE TABLE IF NOT EXISTS "film_genre"
+CREATE TABLE IF NOT EXISTS film_genre
 (
-    "genre_id" bigint,
-    "film_id"  bigint,
-    PRIMARY KEY ("genre_id", "film_id")
+    genre_id bigint,
+    film_id  bigint,
+    PRIMARY KEY (genre_id, film_id)
 );
