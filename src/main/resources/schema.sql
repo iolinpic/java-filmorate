4b5ae2a2174ed432344b3a4acb069a4f8d98451a
iolinpic <iolinpic@gmail.com>
2024-10-14 15:56:15 +0300

    обновление схемы, исключены alters

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,9 +9,15 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS user_friend
 (
-    user_id   bigint,
-    friend_id bigint,
+    user_id   bigint REFERENCES users (id),
+    friend_id bigint REFERENCES users (id),
     PRIMARY KEY (user_id, friend_id)
+);
+
+CREATE TABLE IF NOT EXISTS rating
+(
+    id   bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar
 );
 
 CREATE TABLE IF NOT EXISTS films
@@ -21,52 +27,26 @@ CREATE TABLE IF NOT EXISTS films
     description text,
     releaseDate timestamp,
     duration    int,
-    rating_id   bigint
+    rating_id   bigint REFERENCES rating (id)
 );
 
 CREATE TABLE IF NOT EXISTS film_like
 (
-    user_id bigint,
-    film_id bigint,
+    user_id bigint REFERENCES users (id),
+    film_id bigint REFERENCES films (id),
     PRIMARY KEY (user_id, film_id)
 );
 
 CREATE TABLE IF NOT EXISTS genre
 (
-    id  bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name varchar
-);
-
-CREATE TABLE IF NOT EXISTS rating
-(
     id   bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name varchar
 );
 
+
 CREATE TABLE IF NOT EXISTS film_genre
 (
-    genre_id bigint,
-    film_id  bigint,
+    genre_id bigint REFERENCES genre (id),
+    film_id  bigint REFERENCES films (id),
     PRIMARY KEY (genre_id, film_id)
 );
-
-ALTER TABLE film_like
-    ADD CONSTRAINT user_like FOREIGN KEY (user_id) REFERENCES users (id);
-
-ALTER TABLE film_like
-    ADD CONSTRAINT film_like FOREIGN KEY (film_id) REFERENCES films (id);
-
-ALTER TABLE user_friend
-    ADD CONSTRAINT user_friend FOREIGN KEY (user_id) REFERENCES users (id);
-
-ALTER TABLE user_friend
-    ADD CONSTRAINT friend_user FOREIGN KEY (friend_id) REFERENCES users (id);
-
-ALTER TABLE films
-    ADD CONSTRAINT film_rating FOREIGN KEY (rating_id) REFERENCES rating (id);
-
-ALTER TABLE film_genre
-    ADD FOREIGN KEY (genre_id) REFERENCES genre (id);
-
-ALTER TABLE film_genre
-    ADD FOREIGN KEY (film_id) REFERENCES films (id);
